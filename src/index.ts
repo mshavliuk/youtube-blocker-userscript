f329f4bb31d033519c60fb98e41cb488788aa188
mshavliuk <mikhail.shavliuk@yandex.ru>
2020-10-03 21:35:52 +0300

    clear the main index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,77 +1,33 @@
-import 'reflect-metadata';
+import "reflect-metadata";
+
 import { Clock } from "./clock";
-import { Settings, SettingsData } from "./settings";
-import { Modal } from "./modal";
-import { Container } from 'typedi';
+import { Settings } from "./settings";
+import { Container } from "typedi";
 import { WINDOW_TOKEN } from "./window-token";
+import { Blocker } from "./blocker";
 
-(async function (window) {
-	Container.set(WINDOW_TOKEN, window);
+(async function(window) {
+  await (() => new Promise((resolve) => {
+    const timer = (counter = 0) => {
+      if (window.document.body) {
+        resolve();
+      } else {
+        setTimeout(() => timer(counter), ++counter * 50);
+      }
+    };
+    timer();
+  }))();
 
-	const waitForBody = () => {
-		let counter = 0;
-		return new Promise((resolve) => {
-			const timer = () => {
-				if (window.document.body) {
-					resolve();
-				} else {
-					counter++;
-					setTimeout(timer, counter * 50);
-				}
-			};
-			timer();
-		});
-	};
+  Container.set(WINDOW_TOKEN, window);
+  const clock = Container.get(Clock);
+  window.addEventListener("beforeunload", () => {
+    clock.stop();
+  });
+  const settings = Container.get(Settings);
+  if (!settings.isSettingsSpecified()) {
+    await settings.showSettingsDialog();
+  }
 
-	await waitForBody();
+  Container.get(Blocker);
 
-	const main = async () => {
-		const settings = Container.get(Settings);
-		let settingsData: SettingsData | null = null;
-		if(!settings.isSettingsSpecified()) {
-			settingsData = await settings.showSettingsDialog();
-		}
-		if(settingsData === null) {
-			return;
-		}
-
-		const clock = Container.get(Clock);
-		window.addEventListener("beforeunload", () => {
-			clock.stop();
-		});
-
-		const todayDateString = new Date().toISOString().slice(0, 10);
-
-		const scheduleBlockStart = settingsData.startTime;
-		const scheduleBlockStop = settingsData.endTime;
-
-		const blockTimeStart = Date.parse(
-			`${todayDateString}T${scheduleBlockStart}`
-		);
-		const blockTimeStop = Date.parse(`${todayDateString}T${scheduleBlockStop}`);
-
-		const timeSinceStartOfBlock = Date.now() - blockTimeStart;
-		const timeUntilEndOfBlock = blockTimeStop - Date.now();
-
-		// const destroyThePage = () => {
-		// 	window.document.body.innerHTML = "";
-		// };
-
-		const blockTheSite = () => {
-			// destroyThePage();
-			const modal = Container.get(Modal);
-			modal.show(
-				"Let's get a grip!",
-				"You seem to visit this site in restricted time"
-			);
-		};
-
-		if (timeSinceStartOfBlock > 0 && timeUntilEndOfBlock > 0) {
-			blockTheSite();
-		} else if (timeSinceStartOfBlock < 0) {
-			setTimeout(blockTheSite, -timeSinceStartOfBlock);
-		}
-	};
-
-	main();
 })(typeof unsafeWindow !== "undefined" ? unsafeWindow : window);
