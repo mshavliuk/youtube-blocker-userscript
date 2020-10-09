384da6915c9faccf8e60e9f1926d645c17f383d4
mshavliuk <mikhail.shavliuk@yandex.ru>
2020-10-09 19:35:13 +0300

    add break duration settings

--- src/settings/settings.ts
+++ src/settings/settings.ts
@@ -9,12 +9,22 @@ export type SettingsData = {
 	startTime: string | null;
 	endTime: string | null;
 	dailyLimit: number | null;
+	breakAllowed: boolean;
+	breakDuration: number | null;
 };
 
 @Service()
 export class Settings {
 	public readonly prefix = `${HTML_PREFIX}__settings`;
 	private readonly storeKey = `${STORE_PREFIX} Settings`;
+	private readonly defaults: SettingsData = {
+		days: [],
+		startTime: null,
+		endTime: null,
+		dailyLimit: null,
+		breakAllowed: false,
+		breakDuration: null,
+	};
 
 	constructor(
 		@Inject(WINDOW_TOKEN) private window: Window,
@@ -53,43 +63,67 @@ export class Settings {
 		const templateElement = document.createElement("div");
 		templateElement.innerHTML = template(this);
 
-		return new Promise((resolve) => {
-			this.modal.show("Settings", templateElement).then(
-				// TODO: add form validation
-				(result) => {
-					if (!result) {
-						resolve(null);
-						return;
-					}
+		const form = templateElement.querySelector(
+			`#${this.prefix}`
+		) as HTMLFormElement;
 
-					const form = templateElement.querySelector(`#${this.prefix}`);
-					const formData = new FormData(form as HTMLFormElement);
-					const strData = Array.from(formData.entries())
-						.filter((v) => !!v)
-						.reduce(
-							(res: { [k: string]: string | string[] }, [key, value]) => ({
-								...res,
-								[key]: res[key]
-									? [String(value)].concat(res[key])
-									: String(value),
-							}),
-							{}
-						) as {
-						[k in keyof SettingsData]?: SettingsData[k] extends Array<unknown>
-							? string[]
-							: string;
-					};
-					const data: SettingsData = {
-						endTime: null,
-						startTime: null,
-						...strData,
-						dailyLimit: Number(strData.dailyLimit) || null,
-						days: Array.from(strData.days ?? []).map(Number),
-					};
-					this.setSettings(data);
-					resolve(data);
-				}
+		this.bindFormInteraction(form);
+
+		return new Promise((resolve) => {
+			this.modal.show({ title: "Settings", content: templateElement }).then(
+				// TODO: add form validation
+				(result) => resolve(this.onSettingsSubmit(result, form))
 			);
 		});
+	}
+
+	private bindFormInteraction(form: HTMLFormElement) {
+		const breakCheckbox = form.querySelector(
+			'[name="breakAllowed"]'
+		) as HTMLInputElement;
+		breakCheckbox.addEventListener("change", () => {
+			const breakDurationField = form.querySelector(
+				`#${this.prefix}__break-field`
+			)!;
+			if (breakCheckbox.checked) {
+				breakDurationField.setAttribute("style", "display: block");
+			} else {
+				breakDurationField.setAttribute("style", "display: none");
+			}
+		});
+	}
+
+	private onSettingsSubmit(
+		result: boolean,
+		form: HTMLFormElement
+	): null | SettingsData {
+		if (!result) {
+			return null;
+		}
+
+		const formData = new FormData(form);
+		const strData = Array.from(formData.entries())
+			.filter((v) => !!v)
+			.reduce(
+				(res: { [k: string]: string | string[] }, [key, value]) => ({
+					...res,
+					[key]: res[key] ? [String(value)].concat(res[key]) : String(value),
+				}),
+				{}
+			) as {
+			[k in keyof SettingsData]?: SettingsData[k] extends Array<unknown>
+				? string[]
+				: string;
+		};
+		const data: SettingsData = {
+			...this.defaults,
+			...strData,
+			breakAllowed: strData.breakAllowed === "on",
+			breakDuration: Number(strData.breakDuration) || null,
+			dailyLimit: Number(strData.dailyLimit) || null,
+			days: Array.from(strData.days ?? []).map(Number),
+		};
+		this.setSettings(data);
+		return data;
 	}
 }
