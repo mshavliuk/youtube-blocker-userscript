34ae09d89e03070824b200e45f473214ea447b93
mshavliuk <mikhail.shavliuk@yandex.ru>
2020-10-10 11:14:39 +0300

    bump script version

--- src/script-header.js
+++ src/script-header.js
@@ -5,7 +5,7 @@
 // @description Block youtube usage by preset rules
 // @author      mshavliuk
 // @license     MIT; https://opensource.org/licenses/MIT
-// @version     0.1
+// @version     0.2
 // @icon        https://www.youtube.com/s/desktop/aa71f599/img/favicon_144.png
 // @updateURL   https://raw.githubusercontent.com/mshavliuk/uerscripts/master/youtube-blocker.js
 // @grant       none
