09d5b86da797ecf46a7032295627f7a21f9e6569
mshavliuk <mshavliuk@users.noreply.github.com>
2021-01-09 09:59:51 +0200

    add project icon

--- src/script-header.js
+++ src/script-header.js
@@ -2,11 +2,11 @@
 // @id          Youtube Blocker@https://github.com/mshavliuk/youtube-blocker-userscript
 // @name        Youtube Blocker
 // @namespace   https://github.com/mshavliuk/youtube-blocker-userscript
-// @description Block youtube usage by preset rules
+// @description Block youtube usage by configured rules
 // @author      mshavliuk
 // @license     MIT; https://opensource.org/licenses/MIT
 // @version     0.4.0
-// @icon        https://www.youtube.com/s/desktop/aa71f599/img/favicon_144.png
+// @icon        https://raw.githubusercontent.com/mshavliuk/youtube-blocker-userscript/master/icon.png
 // @updateURL   https://github.com/mshavliuk/youtube-blocker-userscript/releases/download/latest/userscript.js
 // @grant       none
 // @run-at      document-start
