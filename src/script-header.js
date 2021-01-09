6d6c697d5694d8d8bbaac5e8c2abaf48f447cb66
mshavliuk <mshavliuk@users.noreply.github.com>
2021-01-09 10:00:25 +0200

    bump version to 0.5.0

--- src/script-header.js
+++ src/script-header.js
@@ -5,7 +5,7 @@
 // @description Block youtube usage by configured rules
 // @author      mshavliuk
 // @license     MIT; https://opensource.org/licenses/MIT
-// @version     0.4.0
+// @version     0.5.0
 // @icon        https://raw.githubusercontent.com/mshavliuk/youtube-blocker-userscript/master/icon.png
 // @updateURL   https://github.com/mshavliuk/youtube-blocker-userscript/releases/download/latest/userscript.js
 // @grant       none
